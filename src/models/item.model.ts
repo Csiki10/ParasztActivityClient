d02ac658cd4b5b0cbcb2f1aacb4d6627369c4b1d
benedek.csikos <csikosbenedek@gmail.com>
2024-10-29 21:42:20 +0100

    init base app

---
+++ src/models/item.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Item extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'boolean',
+    required: false,
+    default: true,
+  })
+  active?: boolean;
+
+  constructor(data?: Partial<Item>) {
+    super(data);
+  }
+}
+
+export interface ItemRelations {
+  // describe navigational properties here
+}
+
+export type ItemWithRelations = Item & ItemRelations;
