fd71e3ff1824ea897a177af0a3759a8e25cc416c
benedek.csikos <csikosbenedek@gmail.com>
2024-11-03 22:53:30 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NewGameComponent } from './features/new-game/pages/new-game/new-game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NewGameComponent },
+  { path: 'new-game', component: NewGameComponent },
+  { path: '**', redirectTo: 'new-game' },
+];
